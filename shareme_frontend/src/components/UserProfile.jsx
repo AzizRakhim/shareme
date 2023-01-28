d9f32b4dea99cda3bca3db9e896b0f8122e2c256
Aziz Rakhim <azizbekrahimjonov80@gmail.com>
2023-01-28 23:57:45 +0500

    feat: finished the project

--- shareme_frontend/src/components/UserProfile.jsx
+++ shareme_frontend/src/components/UserProfile.jsx
@@ -12,6 +12,12 @@ import { client } from "../client";
 import MasonryLayout from "./MasonryLayout";
 import Spinner from "./Spinner";
 
+const activeBtnStyles =
+  "bg-red-500 text-white font-bold p-2 rounded-full w-20 outline-none";
+
+const notActiveBtnStyles =
+  "bg-primary mr-4 text-black font-bold p-2 rounded-full w-20 outline-none";
+
 const randomImage =
   "https://source.unsplash.com/1600x900/?nature,photography,technology";
 
@@ -30,6 +36,22 @@ function UserProfile() {
       setUser(data[0]);
     });
   }, [userId]);
+
+  useEffect(() => {
+    if (text === "Created") {
+      const createdPinsQuery = userCreatedPinsQuery(userId);
+
+      client.fetch(createdPinsQuery).then((data) => {
+        setPins(data);
+      });
+    } else {
+      const savedPinsQuery = userSavedPinsQuery(userId);
+
+      client.fetch(savedPinsQuery).then((data) => {
+        setPins(data);
+      });
+    }
+  }, [text, userId]);
 
   if (!user) {
     return <Spinner message={"Loading profile..."} />;
@@ -70,6 +92,41 @@ function UserProfile() {
               )}
             </div>
           </div>
+          <div className="text-center mb-7">
+            <button
+              type="button"
+              onClick={(e) => {
+                setText(e.target.textContent);
+                setActiveBtn("created");
+              }}
+              className={`${
+                activeBtn === "created" ? activeBtnStyles : notActiveBtnStyles
+              }`}
+            >
+              Created
+            </button>
+            <button
+              type="button"
+              onClick={(e) => {
+                setText(e.target.textContent);
+                setActiveBtn("saved");
+              }}
+              className={`${
+                activeBtn === "saved" ? activeBtnStyles : notActiveBtnStyles
+              }`}
+            >
+              Saved
+            </button>
+          </div>
+          {pins?.length ? (
+            <div className="px-2">
+              <MasonryLayout pins={pins} />
+            </div>
+          ) : (
+            <div className="flex justify-center font-bold items-center w-full text-xl mt-2">
+              No Pins Found!
+            </div>
+          )}
         </div>
       </div>
     </div>
