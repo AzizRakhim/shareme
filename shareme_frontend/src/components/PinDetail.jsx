34aab630cf622ffb019fad01f8af6a3d4723eeb0
Aziz Rakhim <azizbekrahimjonov80@gmail.com>
2023-01-28 17:24:55 +0500

    refactor: pinDetail page finished and userProfile page started

--- shareme_frontend/src/components/PinDetail.jsx
+++ shareme_frontend/src/components/PinDetail.jsx
@@ -3,7 +3,7 @@ import { MdDownloadForOffline } from "react-icons/md";
 import { Link, useParams } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 
-import { client } from "../client";
+import { client, urlFor } from "../client";
 import MasonryLayout from "./MasonryLayout";
 import { pinDetailMorePinQuery, pinDetailQuery } from "../utils/data";
 import Spinner from "./Spinner";
@@ -14,6 +14,32 @@ function PinDetail({ user }) {
   const [comment, setComment] = useState("");
   const [addingComment, setAddingComment] = useState(false);
   const { pinId } = useParams();
+
+  const addComment = () => {
+    if (comment) {
+      setAddingComment(true);
+
+      client
+        .patch(pinId)
+        .setIfMissing({ comments: [] })
+        .insert("after", "comments[-1]", [
+          {
+            comment,
+            _key: uuidv4(),
+            postedBy: {
+              _type: "postedBy",
+              _ref: user._id,
+            },
+          },
+        ])
+        .commit()
+        .then(() => {
+          fetchPinDetails();
+          setComment("");
+          setAddingComment(false);
+        });
+    }
+  };
 
   const fetchPinDetails = () => {
     let query = pinDetailQuery(pinId);
@@ -37,7 +63,112 @@ function PinDetail({ user }) {
 
   if (!pinDetail) return <Spinner message={"Loading pin..."} />;
 
-  return <div>PinDetail</div>;
+  return (
+    <>
+      <div
+        className="flex xl-flex-row flex-col m-auto bg-whtie"
+        style={{ maxWidth: "1500px", borderRadius: "32px" }}
+      >
+        <div className="flex justify-center items-center md:items-start flex-initial">
+          <img
+            src={pinDetail?.image && urlFor(pinDetail.image).url()}
+            alt="user-post"
+            className="rounded-t-3xl rounded-b-lg"
+          />
+        </div>
+        <div className="w-full p-5 flex-1 xl:min-w-620">
+          <div className="flex items-center justify-between">
+            <div className="flex gap-2 items-center">
+              <a
+                href={`${pinDetail.image?.asset?.url}?dl=`}
+                download
+                onClick={(e) => e.stopPropagation()}
+                className="bg-white w-9 h-9 rounded-full flex items-center justify-center text-dark text-xl opacity-75 hover:opacity-100 hover:shadow-md outline-none"
+              >
+                <MdDownloadForOffline />
+              </a>
+            </div>
+            <a href={pinDetail.destination} target="_blank" rel="noreferrer">
+              {pinDetail.destination.length > 15
+                ? `${pinDetail.destination.slice(0, 15)}...`
+                : pinDetail.destination}
+            </a>
+          </div>
+          <div>
+            <h1 className="text-4xl font-bold break-words mt-3">
+              {pinDetail.title}
+            </h1>
+            <p className="mt-3">{pinDetail.about}</p>
+          </div>
+          <Link
+            to={`user-profile/${pinDetail.postedBy?._id}`}
+            className="flex gap-2 mt-5 items-center bg-white rounded-lg"
+          >
+            <img
+              src={pinDetail.postedBy?.image}
+              alt="user-profile"
+              className="w-8 h-8 rounded-full object-cover"
+            />
+            <p className="font-semibold capitalize">
+              {pinDetail.postedBy?.userName}
+            </p>
+          </Link>
+          <h2 className="mt-5 text-2xl">Comments</h2>
+          <div className="max-h-370 overflow-y-auto">
+            {pinDetail?.comments?.map((comment, i) => (
+              <div
+                className="flex gap-2 mt-5 items-center bg-white rounded-lg"
+                key={i}
+              >
+                <img
+                  src={comment.postedBy.image}
+                  alt="user-profile"
+                  className="w-10 h-10 rounded-full cursor-pointer"
+                />
+                <div className="flex flex-col">
+                  <p className="font-bold">{comment.postedBy.userName}</p>
+                  <p>{comment.comment}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="flex flex-wrap mt-6 gap-3">
+            <Link to={`user-profile/${pinDetail.postedBy?._id}`}>
+              <img
+                src={pinDetail.postedBy?.image}
+                alt="user-profile"
+                className="w-10 h-10 rounded-full cursor-pointer"
+              />
+            </Link>
+            <input
+              type="text"
+              className="flex-1 border-gray-100 outline-none border-2 p-2 rounded-2xl focus:border-gray-300"
+              placeholder="Add a comment"
+              value={comment}
+              onChange={(e) => setComment(e.target.value)}
+            />
+            <button
+              type="button"
+              className="bg-red-500 text-white rounded-full px-6 py-2 font-semibold text-base outline-none"
+              onClick={addComment}
+            >
+              {addingComment ? "Posting the comment..." : "Post"}
+            </button>
+          </div>
+        </div>
+      </div>
+      {pins?.length ? (
+        <>
+          <h2 className="text-center font-bold text-2xl mt-8 mb-4">
+            More like this
+          </h2>
+          <MasonryLayout pins={pins} />
+        </>
+      ) : (
+        <Spinner message={"Loding more pins..."} />
+      )}
+    </>
+  );
 }
 
 export default PinDetail;
--- shareme_frontend/src/components/UserProfile.jsx
+++ shareme_frontend/src/components/UserProfile.jsx
@@ -1,7 +1,79 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { AiOutlineLogout } from "react-icons/ai";
+import { useParams, useNavigate } from "react-router-dom";
+import { googleLogout } from "@react-oauth/google";
+
+import {
+  userCreatedPinsQuery,
+  userQuery,
+  userSavedPinsQuery,
+} from "../utils/data";
+import { client } from "../client";
+import MasonryLayout from "./MasonryLayout";
+import Spinner from "./Spinner";
+
+const randomImage =
+  "https://source.unsplash.com/1600x900/?nature,photography,technology";
 
 function UserProfile() {
-  return <div>UserProfile</div>;
+  const [user, setUser] = useState(null);
+  const [pins, setPins] = useState(null);
+  const [text, setText] = useState("Created");
+  const [activeBtn, setActiveBtn] = useState("created");
+  const navigate = useNavigate();
+  const { userId } = useParams();
+
+  useEffect(() => {
+    const query = userQuery(userId);
+
+    client.fetch(query).then((data) => {
+      setUser(data[0]);
+    });
+  }, [userId]);
+
+  if (!user) {
+    return <Spinner message={"Loading profile..."} />;
+  }
+
+  return (
+    <div className="relative pb-2 h-full justify-center items-center">
+      <div className="flex flex-col pb-5">
+        <div className="relative flex flex-col mb-7">
+          <div className="flex flex-col justify-center items-center">
+            <img
+              src={randomImage}
+              alt="banner-pic"
+              className="w-full h-370 2xl:h-510 shodow-lg object-cover"
+            />
+            <img
+              src={user.image}
+              alt="user-pic"
+              className="rounded-full w-20 h-20 -mt-10 shadow-xl object-cover"
+            />
+            <h1 className="font-bold text-3xl text-center mt-3">
+              {user.userName}
+            </h1>
+            <div className="absolute top-0 z-1 right-0 p-2">
+              {userId === user._id && (
+                <button
+                  type="button"
+                  className="bg-white p-2 rounded-full cursor-pointer outline-non shadow-md"
+                  onClick={() => {
+                    googleLogout();
+                    localStorage.clear();
+
+                    navigate("/login");
+                  }}
+                >
+                  <AiOutlineLogout color="red" fontSize={21} />
+                </button>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default UserProfile;
